eb634784601ca8a7f1cbea448180105afb8c4d72
Tarak chakraborty <144036469+Tarak-Chakraborty@users.noreply.github.com>
2024-05-10 07:48:51 +0530

    Update StoreContext.jsx

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://backend-safolloacademy.onrender.com";
+  const url = "https://backend-safolloacademy.onrender.com";
   const [token,setToken] = useState("")
   const [book_list,setBookList] = useState([])
 
